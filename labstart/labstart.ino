5282cabfe5eda5ae8ddc9146c33d4ef30bc36cc9
lp17917 <79586788+lp17917@users.noreply.github.com>
2021-05-10 15:54:30 +0100

    Update labstart.ino

--- labstart/labstart.ino
+++ labstart/labstart.ino
@@ -3,7 +3,7 @@
 
 #include "encoders.h"
 #include "motor.h"
-
+#include "pid.h"
 
 #define L_PWM_PIN 10
 #define L_DIR_PIN 16
@@ -14,26 +14,66 @@
 
 VL6180X sensor;
 
+
+float Kp_left = 0.05; //Proportional gain 
+float Kd_left = 0.0; //Derivative gain
+float Ki_left = 0.0001; //Integral gain
+PID_c left_PID(Kp_left, Ki_left, Kd_left); // controller for left wheel
+
+float Kp_right = 0.05; //Proportional gain 
+float Kd_right = 0.0; //Derivative gain
+float Ki_right = 0.0001; //Integral gain
+PID_c right_PID(Kp_right, Ki_right, Kd_right); // controller for right wheel
+
 motor_c left_motor(L_DIR_PIN,L_PWM_PIN);
 motor_c right_motor(R_DIR_PIN,R_PWM_PIN);
 
+unsigned long timestamp;
+unsigned long speedstamp;
+int leftpre;
+int rightpre;
+int leftstart;
+int rightstart;
 int right;
 int left;
-long leftstamp;
-long rightstamp;
+float desspeed;
+float demand;
+float rightvel;
+float leftvel;
+float leftoutput;
+float rightoutput;
+float distance;
 bool fin;
+
+void speedcalc(unsigned long timechange){
+  int leftchange = count_e0 - leftpre;
+  int rightchange = count_e1 - rightpre;
+  leftvel = (float)leftchange/(float)timechange;
+  rightvel = (float)rightchange/(float)timechange;
+  leftpre = count_e0;
+  rightpre = count_e1;
+  
+}
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
- 
-  leftstamp = get_e0;
-  rightstamp = get_e1;
-  left = 30;
-  right = 30;
-  fin = false;
+  left = 60;
+  right = 60;
   setupEncoder0();
   setupEncoder1();
+  fin = false;
+  left_motor.speedchange(left);
+  right_motor.speedchange(right);
+  timestamp = millis();
+  leftoutput = 0;
+  rightoutput = 0;
+  leftpre = count_e0;
+  rightpre = count_e1;
+  leftstart = count_e0;
+  rightstart = count_e1;
+  desspeed = 0.2;
+  demand = (((desspeed/0.21991)*1440)/1000);
   sensor.init();
   sensor.configureDefault();
   sensor.setScaling(SCALING);
@@ -42,19 +82,35 @@ void setup() {
 
 void loop() {
 
+
+  unsigned long timechange = millis() - timestamp;
+  unsigned long distchange = millis() - speedstamp;
   if (sensor.readRangeSingleMillimeters() > 100 && !fin){
-      right = drive_straight(leftstamp,rightstamp, left);
-      left_motor.speedchange(left);
-      right_motor.speedchange(right);
+    if (timechange > 100){
+      speedcalc(timechange);
+      rightoutput = right_PID.update(demand, rightvel);
+      leftoutput = left_PID.update(demand, leftvel);
+      timestamp = millis();
+      
+    
+      left_motor.speedchange(leftoutput*255);
+      right_motor.speedchange(rightoutput*255);
+      //Serial.print(leftoutput);
+      //Serial.print(",");
+      //Serial.println(rightoutput);
+    }
+
   }
+  
   else{
     fin = true;
-     left_motor.speedchange(0);
-     right_motor.speedchange(0);
+    left_motor.speedchange(0);
+    right_motor.speedchange(0);
+    if (distchange > 1000){
+      Serial.println(((float)count_e1-rightstart)*(0.219911/1440));
+      speedstamp = millis();
+    }
   }
-  Serial.print(count_e0);
-  Serial.print(",");
-  Serial.print(count_e1);
-  Serial.println();
 
+//Serial.println(sensor.readRangeSingleMillimeters());
 }
